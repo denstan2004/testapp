64d8766714e9a6bf57791d4cc6a103c40dc5a267
Денис Станіслав <125273149+denstan2004@users.noreply.github.com>
2023-10-09 11:18:52 +0300

    added cors policy

--- testapp/Program.cs
+++ testapp/Program.cs
@@ -38,7 +38,7 @@ if (!app.Environment.IsDevelopment())
     });
 }
 app.UseCors(builder => builder
-    .WithOrigins("http://localhost:3000") // Розділіть по комах, якщо потрібно додати більше доменів
+    .WithOrigins("*") // Розділіть по комах, якщо потрібно додати більше доменів http://localhost:3000
     .AllowAnyHeader()
     .AllowAnyMethod()
 );
