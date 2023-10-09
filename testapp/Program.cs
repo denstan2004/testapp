8e4abe564b78e3a243b7eb472c05690d1bd2aa87
Денис Станіслав <125273149+denstan2004@users.noreply.github.com>
2023-10-09 15:30:37 +0300

    4

--- testapp/Program.cs
+++ testapp/Program.cs
@@ -38,7 +38,7 @@ if (!app.Environment.IsDevelopment())
     });
 }
 app.UseCors(builder => builder
-    .WithOrigins("*") // Розділіть по комах, якщо потрібно додати більше доменів http://localhost:3000
+    .WithOrigins("*") // Розділіть по комах, якщо потрібно додати більше доменів 
     .AllowAnyHeader()
     .AllowAnyMethod()
 );
