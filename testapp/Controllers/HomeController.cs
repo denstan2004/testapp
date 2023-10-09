4379172a706d8da29ba9723dbf4aa865a679e7d4
Денис Станіслав <125273149+denstan2004@users.noreply.github.com>
2023-10-09 17:06:16 +0300

    20

--- testapp/Controllers/HomeController.cs
+++ testapp/Controllers/HomeController.cs
@@ -4,19 +4,21 @@ using testapp.DAL.Repositories;
 namespace testapp.Controllers
 {
     [Route("Home")]
-    public class HomeController : Controller
+    public class HomeController:Controller
     {
         private readonly UserRepository userRepository;
 
         public HomeController(UserRepository userRepository)
-        {
+        {    
             this.userRepository = userRepository;
         }
 
         [HttpGet("getAll/Clothes")]
         public List<int> getallInt()
         {
-            return userRepository.SelectAll();
+            List<int> result = new List<int>();
+            result.Add(1);
+            return result;
         }
 
     }
